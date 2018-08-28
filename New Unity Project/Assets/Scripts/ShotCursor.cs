958638ce18c955a219184e604da875fea4f1342d
Olivia <wild_hosikuzu@icloud.com>
2018-08-28 13:50:27 +0900

    cursor add

---
+++ New Unity Project/Assets/Scripts/ShotCursor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotCursor : MonoBehaviour {
+
+    //　カーソルに使用するテクスチャ
+    [SerializeField]
+    private Texture2D cursor;
+
+    void Start()
+    {
+        //　カーソルを自前のカーソルに変更
+        Cursor.SetCursor(cursor, new Vector2(cursor.width / 2, cursor.height / 2), CursorMode.ForceSoftware);
+    }
+
+    void Update()
+    {
+        //　マウスの左クリックで撃つ
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shot();
+        }
+    }
+
+    //　敵を撃つ
+    void Shot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Enemy")))
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    float speed = 10;
+	// Use this for initialization
+
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+}
